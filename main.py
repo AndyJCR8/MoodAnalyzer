095dc863c9262b0710c109004561a088034ee5bc
AndyJCR8 <andy_cr@umes.edu.gt>
2023-05-29 07:04:18 -0600

    Inicios de la GUI

---
+++ main.py
@@ -0,0 +1,66 @@
+from typing import Optional, Tuple, Union
+import cv2
+import customtkinter as ctk
+from PIL import Image, ImageTk
+
+class TrainFrame(ctk.CTkFrame):
+  def __init__(self, master, **kwargs):
+      super().__init__(master, **kwargs)
+      
+      self.label = ctk.CTkLabel(self, text="TrainFrame label")
+      self.label.place(relx=0.5, rely=0.05, anchor="center")
+      
+class NormalFrame(ctk.CTkFrame):
+  def __init__(self, master, **kwargs):
+      super().__init__(master, **kwargs)
+      
+      self.label = ctk.CTkLabel(self, text="NormalFrame label")
+      self.label.place(relx=0.5, rely=0.05, anchor="center")
+
+class App(ctk.CTk):
+    def __init__(self, title, size):
+        super().__init__()
+        self.title(title)
+        self.geometry(f"{size[0]}x{size[1]}")
+        self.minsize(size[0], size[1])
+        
+        # add widgets to app
+        self.lblTitle = ctk.CTkLabel(self, text="Analizador de emociones MoodAnalizer", font=("", 18))
+        self.lblTitle.pack()
+        
+        self.switchVar = ctk.StringVar(value="on")
+        self.switchMode = ctk.CTkSwitch(self, text="Modo entrenamiento", variable=self.switchVar, command=self.toggleMode, onvalue="on", offvalue="off")
+        self.switchMode.pack()
+        
+        self.modeFrame = NormalFrame(master=self, width=500, height=500)
+        
+        self.video = cv2.VideoCapture(0)
+        self.canvas = ctk.CTkCanvas(self, width=self.video.get(cv2.CAP_PROP_FRAME_WIDTH), height=self.video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.canvas.pack()
+        
+        self.update_frame()
+        self.mainloop()
+    
+    def toggleMode(self):
+      if self.switchVar.get() == "on":
+        self.modeFrame = TrainFrame(master=self, width=500, height=500)
+      else: self.modeFrame = NormalFrame(master=self, width=500, height=500)
+    
+    def update_frame(self):
+        self.lblTitle.place(relx=0.5, rely=0.05, anchor="center")
+        self.switchMode.place(relx=0.8, rely=0.1, anchor="center")
+        self.modeFrame.place(relx=0.77, rely=0.55, anchor="center")
+        
+        ret, frame = self.video.read()
+        frame = cv2.flip(frame, 1)
+        if ret:
+            cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image = Image.fromarray(cv2image)
+            
+            self.photo = ImageTk.PhotoImage(image=image)
+            self.canvas.create_image(0, 0, anchor=ctk.NW, image=self.photo)
+            self.canvas.place(relx=0.28, rely=0.55, anchor="center")
+            
+        self.after(15, self.update_frame)
+
+App("Mood Analizer", (1200, 600))
